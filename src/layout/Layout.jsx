0bf96c759929c65379cab670ecf4bf6d854bebc8
R10F <farrellriofa@gmail.com>
2022-07-02 15:36:15 +0700

    merapikan

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -7,9 +7,9 @@ class Layout extends React.Component {
       <main>
         <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top">
           <div className="container px-4 px-lg-5">
-            <a className="navbar-brand" href="#!">
+            <Link to="/" className="navbar-brand">
               Samsan Tech
-            </a>
+            </Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
--- src/page/Cart.jsx
+++ src/page/Cart.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 
-class Cart extends React.Component{
-    render(){
-        return(
-            <h1>Cart</h1>
-        )
-    }
+class Cart extends React.Component {
+  render() {
+    return <h1>Cart</h1>;
+  }
 }
 export default Cart;
