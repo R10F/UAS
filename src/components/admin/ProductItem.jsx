4063b07110e129af052c3d1858761644af140a24
fdavidsen <f.davidsen19@gmail.com>
2022-07-04 15:00:28 +0700

    Fix: Modal not showing

--- src/components/admin/ProductItem.jsx
+++ src/components/admin/ProductItem.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 class ProductItem extends React.Component {
   constructor(props) {
@@ -23,87 +23,87 @@ class ProductItem extends React.Component {
     let harga = new Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR", minimumFractionDigits: 0 }).format(this.props.product.harga);
 
     return (
-      <tr>
-        <td>
-          <img src={this.props.product.src} className="w-50" alt={this.props.product.nama} />
-        </td>
-        <td>{this.props.product.nama}</td>
-        <td>{harga}</td>
-        <td>{this.props.product.rating ? this.props.product.rating : "-"}</td>
-        <td>{this.props.product.color}</td>
-        <td>
-          <button className="btn btn-warning" data-bs-toggle="modal" data-bs-target={"#editProductModal-" + this.props.product.id}>
-            Edit
-          </button>
-        </td>
+      <Fragment>
+        <tr>
+          <td>
+            <img src={this.props.product.src} className="w-50" alt={this.props.product.nama} />
+          </td>
+          <td>{this.props.product.nama}</td>
+          <td>{harga}</td>
+          <td>{this.props.product.rating ? this.props.product.rating : "-"}</td>
+          <td>{this.props.product.color}</td>
+          <td>
+            <button className="btn btn-warning" data-bs-toggle="modal" data-bs-target={"#editProductModal-" + this.props.product.id}>
+              Edit
+            </button>
+          </td>
+        </tr>
+        
+        <div className="modal fade" id={"editProductModal-" + this.props.product.id} tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div className="modal-dialog modal-lg">
+            <div className="modal-content">
+              <div className="modal-header text-center">
+                <h5 className="modal-title" id="exampleModalLabel">
+                  Edit Product
+                </h5>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
 
-        <td className="d-none">
-          <div className="modal fade" id={"editProductModal-" + this.props.product.id} tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div className="modal-dialog modal-lg">
-              <div className="modal-content">
-                <div className="modal-header text-center">
-                  <h5 className="modal-title" id="exampleModalLabel">
-                    Edit Product
-                  </h5>
-                  <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              <form onSubmit={this.editProduct}>
+                <div className="modal-body text-start">
+                  <div className="mb-4">
+                    <label htmlFor={"editProductModalInput-1" + this.props.product.id} className="form-label">
+                      Name
+                    </label>
+                    <input type="text" className="form-control" id={"editProductModalInput-1" + this.props.product.id} defaultValue={this.props.product.nama} />
+                  </div>
+
+                  <label htmlFor={"editProductModalInput-2" + this.props.product.id} className="form-label">
+                    Price
+                  </label>
+                  <div className="input-group mb-4">
+                    <span class="input-group-text" id="basic-addon1">
+                      Rp
+                    </span>
+                    <input type="text" className="form-control" id={"editProductModalInput-2" + this.props.product.id} defaultValue={this.props.product.harga} />
+                  </div>
+
+                  <div className="mb-4">
+                    <label htmlFor={"editProductModalInput-3" + this.props.product.id} className="form-label">
+                      Color
+                    </label>
+                    <input type="text" className="form-control" id={"editProductModalInput-3" + this.props.product.id} defaultValue={this.props.product.color} />
+                  </div>
+
+                  <div class="form-check form-switch mb-4">
+                    <input class="form-check-input" type="checkbox" role="switch" id={"editProductModalInput-4" + this.props.product.id} defaultChecked={this.props.product.isSales} />
+                    <label class="form-check-label" for={"editProductModalInput-4" + this.props.product.id}>Sale</label>
+                  </div>
+
+                  <label htmlFor={"editProductModalInput-5" + this.props.product.id} className="form-label">
+                    Sale Price
+                  </label>
+                  <div className="input-group mb-4">
+                    <span class="input-group-text" id="basic-addon1">
+                      Rp
+                    </span>
+                    <input type="text" className="form-control" id={"editProductModalInput-5" + this.props.product.id} defaultValue={this.props.product.salePrice} />
+                  </div>
                 </div>
 
-                <form onSubmit={this.editProduct}>
-                  <div className="modal-body text-start">
-                    <div className="mb-4">
-                      <label htmlFor={"editProductModalInput-1" + this.props.product.id} className="form-label">
-                        Name
-                      </label>
-                      <input type="text" className="form-control" id={"editProductModalInput-1" + this.props.product.id} defaultValue={this.props.product.nama} />
-                    </div>
-
-                    <label htmlFor={"editProductModalInput-2" + this.props.product.id} className="form-label">
-                      Price
-                    </label>
-                    <div className="input-group mb-4">
-                      <span class="input-group-text" id="basic-addon1">
-                        Rp
-                      </span>
-                      <input type="text" className="form-control" id={"editProductModalInput-2" + this.props.product.id} defaultValue={this.props.product.harga} />
-                    </div>
-
-                    <div className="mb-4">
-                      <label htmlFor={"editProductModalInput-3" + this.props.product.id} className="form-label">
-                        Color
-                      </label>
-                      <input type="text" className="form-control" id={"editProductModalInput-3" + this.props.product.id} defaultValue={this.props.product.color} />
-                    </div>
-
-                    <div class="form-check form-switch mb-4">
-                      <input class="form-check-input" type="checkbox" role="switch" id={"editProductModalInput-4" + this.props.product.id} defaultChecked={this.props.product.isSales} />
-                      <label class="form-check-label" for={"editProductModalInput-4" + this.props.product.id}>Sale</label>
-                    </div>
-
-                    <label htmlFor={"editProductModalInput-5" + this.props.product.id} className="form-label">
-                      Sale Price
-                    </label>
-                    <div className="input-group mb-4">
-                      <span class="input-group-text" id="basic-addon1">
-                        Rp
-                      </span>
-                      <input type="text" className="form-control" id={"editProductModalInput-5" + this.props.product.id} defaultValue={this.props.product.salePrice} />
-                    </div>
-                  </div>
-
-                  <div className="modal-footer">
-                    <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">
-                      Close
-                    </button>
-                    <button type="submit" className="btn btn-primary" data-bs-dismiss="modal">
-                      Save changes
-                    </button>
-                  </div>
-                </form>
-              </div>
+                <div className="modal-footer">
+                  <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">
+                    Close
+                  </button>
+                  <button type="submit" className="btn btn-primary" data-bs-dismiss="modal">
+                    Save changes
+                  </button>
+                </div>
+              </form>
             </div>
           </div>
-        </td>
-      </tr>
+        </div>
+      </Fragment>
     );
   }
 }
