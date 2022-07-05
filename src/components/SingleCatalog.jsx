1277ac53638dbbeb82b4065921974ed29203a02d
R10F <farrellriofa@gmail.com>
2022-07-05 14:18:06 +0700

    gitnya ngulah

--- src/components/SingleCatalog.jsx
+++ src/components/SingleCatalog.jsx
@@ -19,34 +19,35 @@ export default function SingleCatalog(props) {
         </div>
         <div className="col-lg-7">
           <h1>{product.nama}</h1>
-          {(product.isSales) ?
-            <h2 className="text-danger">{new Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR", minimumFractionDigits: 0 }).format(product.salePrice)}
+          {product.isSales ? (
+            <h2 className="text-danger">
+              {new Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR", minimumFractionDigits: 0 }).format(product.salePrice)}
               <small className="text-muted text-decoration-line-through fs-4">{new Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR", minimumFractionDigits: 0 }).format(product.harga)}</small>
             </h2>
-            :
+          ) : (
             <h2>{new Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR", minimumFractionDigits: 0 }).format(product.harga)}</h2>
-          }
+          )}
           <p className="my-2 lh-lg">
-
             <Star rating={product.rating} />
             <br />
             <b>Merk: </b> ASUS <br />
             <b>Spesifikasi:</b> <br />
-
           </p>
           <ul>
             {product.deskripsi.map((desc, index) => {
-              return (
-                <li key={"deskripsi" + product.id + index}>
-                  {desc}
-                </li>
-              )
+              return <li key={"deskripsi" + product.id + index}>{desc}</li>;
             })}
           </ul>
           <div>
-            <button className="btn btn-outline-dark text-right" onClick={() => { props.addToCart(product.id, 1) }}>Add to Cart</button>
+            <button
+              className="btn btn-outline-dark text-right"
+              onClick={() => {
+                props.addToCart(product.id, 1);
+              }}
+            >
+              Add to Cart
+            </button>
           </div>
-
         </div>
       </div>
     </section>
